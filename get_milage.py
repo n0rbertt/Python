a043db87d71613837fd2c48180803c46771f3ddb
n0rbertt <xxrelativityxx@gmail.com>
2016-03-27 10:18:30 -0400

    Update get_milage.py

--- get_milage.py
+++ get_milage.py
@@ -1,1 +1,6 @@
+miles = float(input("Enter the total distance of your voyage \n"))
+fuel = float(input("How much fuel was consumed? \n"))
 
+mpg = float(miles) / float(fuel)
+
+print ("Your transport was able to procure: \n" , mpg , "Miles Per Gallon" )
